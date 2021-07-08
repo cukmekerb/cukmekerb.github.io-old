b7cecd2325b2aa071933db1af7f76b9eb6eab21c
cukmekerb <cukmekerb@gmail.com>
2021-07-08 09:03:35 -0700

    go back to youtube feed ):

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var lastv;
 function init() {
-    fetch("https://lstnr.gq/.netlify/functions/rss-to-json?url=https://odysee.com/$/rss/@cukmekerb/19ec35dcb324ee80b86acbee9cf2c613546f6ad6")
+    fetch("https://lstnr.gq/.netlify/functions/rss-to-json?url=https%3A%2F%2Fwww.youtube.com%2Ffeeds%2Fvideos.xml%3Fchannel_id%3DUCzSlF1euionqDthloMy2R-Q")
         .then(a => a.json())
         .then(response => {
             response = response.items;
             for (var i = 0; i < 15; i++) {
-                response[i].thumbnail = response[i].enclosures[0].url;
+                response[i].thumbnail = `https://i.ytimg.com/vi/${response[i].id.replace("yt:video:","")}/mqdefault.jpg`;
                 var current_video = document.getElementById("video-$$INDEX%%").outerHTML;
-                current_video = current_video.replace("$$VURL%%",  response[i].url)
+								current_video = current_video.replace("$$VURL%%", "https:\/\/youtu.be/" + response[i].id.replace("yt:video:",""))
                 current_video = current_video.replace(" hidden", "");
                 current_video = current_video.replace("$$THUM%%", response[i].thumbnail);
                 current_video = current_video.replace("$$INDEX%%", i);
-                current_video = current_video.replace("$$TITLE%%", response[i].title.replace(/^(.{29}[^\s]*).*/, "$1"))
+                current_video = current_video.replace("$$TITLE%%", response[i].title.replace(/^(.{29}[^\s]*).*/, "$1"));
                 if (response[i].description.length > 45) {
-                    current_video = current_video.replace("$$DESK%%", response[i].description.slice(0, 45) + "...")
+                    current_video = current_video.replace("$$DESK%%", response[i].description.slice(0, 45) + "...");
                 }
                 else {
                     current_video = current_video.replace("$$DESK%%", response[i].description);
